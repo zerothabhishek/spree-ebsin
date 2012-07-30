3af6d05f6f9a0b4bac2422a85f53f2d5b4dd111b
abhishek <bigbeliever@gmail.com>
2012-07-30 21:09:30 +0530

    Hack to add coupon code processing while checkout#update happens.

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -9,6 +9,10 @@ CheckoutController.class_eval do
     @payment_method = PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
     if @payment_method && @payment_method.kind_of?(PaymentMethod::Ebsin)
       @order.update_attributes(object_params)
+      if @order.respond_to?(:coupon_code) && @order.coupon_code.present?
+        fire_event('spree.checkout.coupon_code_added', :coupon_code => @order.coupon_code)
+        @order = @order.reload
+      end
       redirect_to gateway_ebsin_path(:gateway_id => @payment_method.id, :order_id => @order.id)
     end
   end
